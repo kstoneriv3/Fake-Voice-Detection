7bc0f784c507e50e8783e9979e55655594bf8340
kstoneriv3 <k.stoneriv3@gmail.com>
2018-12-25 14:47:05 +0100

    rm unnecessary files

--- download.py
+++
@@ -1,5 +0,0 @@
-from google_drive_downloader import GoogleDriveDownloader as gdd
-
-gdd.download_file_from_google_drive(file_id='1ld84zHpPh4_kmKb27sQmV_RquxLSzANf',
-                                    dest_path='./data/obama.zip',
-                                    unzip=True)
